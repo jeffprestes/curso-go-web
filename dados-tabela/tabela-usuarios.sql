26f67a7e367717b2e38d25dad0a441660fb681f8
Jeff Prestes <jeffprestes@gmail.com>
2020-04-30 09:28:52 -0300

    add user de teste

---
+++ dados-tabela/tabela-usuarios.sql
@@ -0,0 +1,13 @@
+insert into logcli_loginclient (
+    logcli_clientname,
+    logcli_clientid,
+    logcli_secret,
+    logcli_role
+  )
+values
+  (
+    'Rui Barbosa',
+    'aguiadehaia@gmail.com',
+    '9f793ea1a594df83e527359eb9b467b893d7bf8400879f35fe0ac14eb459c69aa248d1548458cc2a5eae6aff08a72d5bf2df34762a0b265eabfc955c6bdc7fa9',
+    'admin'
+  );
